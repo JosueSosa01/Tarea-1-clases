bc4efa27a265c5e0a103193ea080865988265d50
JosueSosa01 <117098057+JosueSosa01@users.noreply.github.com>
2025-09-29 23:15:58 -0600

    Add files via upload

---
+++ src/Pruebas.java
@@ -0,0 +1,16 @@
+public class Pruebas {
+    public static void main(String[] args) {
+        // Pruebas de Pilas
+        System.out.println("PRUEBAS DE PILAS");
+        Pilas.main(null);   // Llama al main de Pilas.java
+
+        // Pruebas de Queuee
+        System.out.println("PRUEBAS DE QUEUEE");
+        Queuee.main(null);  // Llama al main de Queuee.java
+
+        // Pruebas de Hash
+        System.out.println("PRUEBAS DE HASH");
+        Hash.main(null);    // Llama al main de Hash.java
+    }
+}
+
